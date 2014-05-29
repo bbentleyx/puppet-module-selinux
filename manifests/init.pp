4ee97650ab44762957793a3295043a5fa6412df3
bbentleyx <bbentleyx@gmail.com>
2014-05-29 14:59:40 -0400

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,10 +6,12 @@ class selinux (
   $mode        = 'disabled',
   $type        = 'targeted',
   $config_file = '/etc/selinux/config',
+  $setlocaldefs = undef
 ) {
 
   validate_re($mode, '^enforcing|permissive|disabled$', "mode is ${mode} and must be either 'enforcing', 'permissive' or 'disabled'.")
   validate_re($type, '^targeted|strict$', "type is ${type} and must be either 'targeted' or 'strict'.")
+  validdate_re($setlocaldefs, '^0|1$', "local defs is ${setlocaldefs} must be either 0 or 1
 
   file { 'selinux_config':
     ensure  => 'file',
